a2722615edda959ae3d36590192221dd0927d1bb
devAlen01 <alenermekuulu@gmail.com>
2024-07-06 14:55:52 +0600

    random card fixed & finished

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,39 +1,32 @@
 import React, { useEffect } from "react";
 import scss from "./Home.module.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { getMovie } from "../../features/actionCreators/getMovie";
-
 import SliderMovie from "../../components/SliderMovie/SliderMovie";
 import MoviePopularCards from "../../components/MoviesCards/MoviesCards";
 import Search from "../../components/Search/Search";
+import RandomMovie from "../../components/RandomMovie/RandomMovie";
 
-
-function Home(props) {
-  const dispatch = useDispatch();
-
-  const { movies } = useSelector((s) => s.movie);
-
-  useEffect(() => {
-    dispatch(getMovie("popular", 5));
-  }, []);
-
+function Home() {
   return (
-    <div id="home">
-      <div className="container">
-        <div className="home">
-          <section>
-            <Search />
-          </section>
-          <section>
-            <MoviePopularCards />
-          </section>
-          <section>
-            <center>
-              <SliderMovie />
-            </center>
+    <>
+      <RandomMovie />
+      <div id="home">
+        <div className="container">
+          <div className="home">
+            <section>
+              <Search />
+            </section>
+            <section>
+              <MoviePopularCards />
+            </section>
+            <section>
+              <center>
+                <SliderMovie />
+              </center>
+            </section>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
